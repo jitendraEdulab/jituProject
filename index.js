846b199d4f8b15e5de82fc30edca99f546c6f103
jitendra jangid <jitendra@edulab.in>
2022-11-16 16:15:25 +0530

    fbhf

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.get('/', (req, res) => {
-  // res.send('name:' + req.query.name + "Roll_no: " + req.query.roll_no + "Address: " + req.query.address)
-  // console.log(req.query.name);
-  res.send("hello world!")
+// app.get('/', (req, res) => {
+//   // res.send('name:' + req.query.name + "Roll_no: " + req.query.roll_no + "Address: " + req.query.address)
+//   // console.log(req.query.name);
+//   res.send("hello world!")
 
-})
+// })
 
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+// app.listen(port, () => {
+//   console.log(`http://localhost:${port}/`)
+// })
+
+console.log(process.argv);
